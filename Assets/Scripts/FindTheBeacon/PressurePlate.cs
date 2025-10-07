c0cc226bbe2752245665f090ed2c751b3bf96e8c
Octavian Tudor <tavi_tudor@yahoo.com>
2025-10-07 16:14:27 +0300

    updates

--- Assets/Scripts/FindTheBeacon/PressurePlate.cs
+++ Assets/Scripts/FindTheBeacon/PressurePlate.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[RequireComponent(typeof(Collider))]
 public class PressurePlate : MonoBehaviour
 {
     [Header("Activator Filter")]
